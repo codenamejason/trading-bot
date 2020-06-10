4370c58f771fd7a68be519e2ca72e720dacf7f0e
codenamejason <codenamejason@outlook.com>
2020-06-10 17:15:01 -0400

    add chainlink

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const web3 = new Web3(new HDWalletProvider(process.env.PRIVATE_KEY, process.env.
 
 // Ropsten DAI
 const DAI_ABI = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"INITIAL_SUPPLY","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"burn","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_value","type":"uint256"}],"name":"burnFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_name","type":"string"},{"name":"_symbol","type":"string"},{"name":"_decimals","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_burner","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Burn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]
-const DAI_ADDRESS = '0xad6d458402f60fd3bd25163575031acdce07538d'
+const DAI_ADDRESS = '0xaD6D458402F60fD3Bd25163575031ACDce07538D'
 const daiContract = new web3.eth.Contract(DAI_ABI, DAI_ADDRESS);
 
 // Ropsten Uniswap Dai Exchange: https://ropsten.etherscan.io/address/0xc0fc958f7108be4060F33a699a92d3ea49b0B5f0
@@ -26,88 +26,477 @@ const EXCHANGE_ABI = [{name:'TokenPurchase',inputs:[{type:'address',name:'buyer'
 const EXCHANGE_ADDRESS = '0xc0fc958f7108be4060F33a699a92d3ea49b0B5f0'
 const exchangeContract = new web3.eth.Contract(EXCHANGE_ABI, EXCHANGE_ADDRESS);
 
+// ChainLink 
+const CHAINLINK_ADDRESS = "0x54F3e442958fa305a9facC6FE7aEd293D05c426E";
+const CHAINLINK_ABI = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_requestId",
+				"type": "bytes32"
+			},
+			{
+				"name": "_payment",
+				"type": "uint256"
+			},
+			{
+				"name": "_callbackFunctionId",
+				"type": "bytes4"
+			},
+			{
+				"name": "_expiration",
+				"type": "uint256"
+			}
+		],
+		"name": "cancelRequest",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_requestId",
+				"type": "bytes32"
+			},
+			{
+				"name": "_change",
+				"type": "int256"
+			}
+		],
+		"name": "fulfillEthereumChange",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_requestId",
+				"type": "bytes32"
+			},
+			{
+				"name": "_market",
+				"type": "bytes32"
+			}
+		],
+		"name": "fulfillEthereumLastMarket",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_requestId",
+				"type": "bytes32"
+			},
+			{
+				"name": "_price",
+				"type": "uint256"
+			}
+		],
+		"name": "fulfillEthereumPrice",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_oracle",
+				"type": "address"
+			},
+			{
+				"name": "_jobId",
+				"type": "string"
+			}
+		],
+		"name": "requestEthereumChange",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_oracle",
+				"type": "address"
+			},
+			{
+				"name": "_jobId",
+				"type": "string"
+			}
+		],
+		"name": "requestEthereumLastMarket",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_oracle",
+				"type": "address"
+			},
+			{
+				"name": "_jobId",
+				"type": "string"
+			}
+		],
+		"name": "requestEthereumPrice",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "withdrawLink",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "requestId",
+				"type": "bytes32"
+			},
+			{
+				"indexed": true,
+				"name": "price",
+				"type": "uint256"
+			}
+		],
+		"name": "RequestEthereumPriceFulfilled",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "requestId",
+				"type": "bytes32"
+			},
+			{
+				"indexed": true,
+				"name": "change",
+				"type": "int256"
+			}
+		],
+		"name": "RequestEthereumChangeFulfilled",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "requestId",
+				"type": "bytes32"
+			},
+			{
+				"indexed": true,
+				"name": "market",
+				"type": "bytes32"
+			}
+		],
+		"name": "RequestEthereumLastMarket",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "previousOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipRenounced",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "id",
+				"type": "bytes32"
+			}
+		],
+		"name": "ChainlinkRequested",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "id",
+				"type": "bytes32"
+			}
+		],
+		"name": "ChainlinkFulfilled",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "id",
+				"type": "bytes32"
+			}
+		],
+		"name": "ChainlinkCancelled",
+		"type": "event"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "changeDay",
+		"outputs": [
+			{
+				"name": "",
+				"type": "int256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "currentPrice",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getChainlinkToken",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "lastMarket",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+const chainLinkContract = new web3.eth.Contract(CHAINLINK_ABI, CHAINLINK_ADDRESS);
+let ethPriceFromLinkOracle
+function insertDecimal(number) {
+  return (number / 100).toFixed(2);
+}
+let priceOfEth = 0;
+let oracle = "0xc99B3D447826532722E41bc36e644ba3479E4365";
+let jobId = "3cff0a3524694ff8834bda9cf9c779a1";
+async function getEthAggPrice() {
+  console.log('Retreiving ETH price from Aggregator...')
+  
+  await chainLinkContract.methods.currentPrice().call().then((res) => {
+    //console.log(res);
+    ethPriceFromLinkOracle = res;
+  });
+  var ethPrice = insertDecimal(ethPriceFromLinkOracle);
+  console.log('Price Of ETH -> ', ethPrice);
+  //setAmountConverted(((Number(amount)) / ethPrice));
+  //console.log(`Amount converted ${amountConverted}`)
+  //console.log(`Current Eth price: ${priceOfEth}`);
+};
+
 // Minimum eth to swap
 const ETH_AMOUNT = web3.utils.toWei('1', 'Ether')
-console.log("Eth Amount", ETH_AMOUNT)
+console.log("Eth Amount", ETH_AMOUNT) // -> logs at start only
 
-const ETH_SELL_PRICE = web3.utils.toWei('200', 'Ether') // 200 Dai a.k.a. $200 USD
+const ETH_SELL_PRICE = web3.utils.toWei('250.00', 'Ether') // -> convert to Dai if falls below this amount
 
 async function sellEth(ethAmount, daiAmount) {
-  // Set Deadline 1 minute from now
-  const moment = require('moment') // import moment.js library
-  const now = moment().unix() // fetch current unix timestamp
-  const DEADLINE = now + 60 // add 60 seconds
-  console.log("Deadline", DEADLINE)
+    // Set Deadline 1 minute from now
+    const moment = require('moment') // import moment.js library
+    const now = moment().unix() // fetch current unix timestamp
+    const DEADLINE = now + 60 // add 60 seconds
+    console.log("Deadline", DEADLINE)
 
-  // Transaction Settings
-  const SETTINGS = {
-    gasLimit: 8000000, // Override gas settings: https://github.com/ethers-io/ethers.js/issues/469
-    gasPrice: web3.utils.toWei('50', 'Gwei'),
-    from: process.env.ACCOUNT, // Use your account here
-    value: ethAmount // Amount of Ether to Swap
-  }
+    // Transaction Settings
+    const SETTINGS = {
+        gasLimit: 8000000, // Override gas settings: https://github.com/ethers-io/ethers.js/issues/469
+        gasPrice: web3.utils.toWei('40', 'Gwei'),
+        from: process.env.ACCOUNT, // Use your account here
+        value: ethAmount // Amount of Ether to Swap
+    }
 
-  // Perform Swap
-  console.log('Performing swap...')
-  let result = await exchangeContract.methods.ethToTokenSwapInput(daiAmount.toString(), DEADLINE).send(SETTINGS)
-  console.log(`Successful Swap: https://ropsten.etherscan.io/tx/${result.transactionHash}`)
+    // Perform Swap
+    console.log('Performing swap...')
+    let result = await exchangeContract.methods.ethToTokenSwapInput(daiAmount.toString(), DEADLINE).send(SETTINGS)
+    console.log(`Successful Swap: https://ropsten.etherscan.io/tx/${result.transactionHash}`)
 }
 
 async function checkBalances() {
-  let balance
+    let balance
 
-  // Check Ether balance swap
-  balance = await web3.eth.getBalance(process.env.ACCOUNT)
-  balance = web3.utils.fromWei(balance, 'Ether')
-  console.log("Ether Balance:", balance)
+    // Check Ether balance swap
+    balance = await web3.eth.getBalance(process.env.ACCOUNT)
+    balance = web3.utils.fromWei(balance, 'Ether')
+    console.log("Ether Balance:", balance)
 
-  // Check Dai balance swap
-  balance = await daiContract.methods.balanceOf(process.env.ACCOUNT).call()
-  balance = web3.utils.fromWei(balance, 'Ether')
-  console.log("Dai Balance:", balance)
+    // Check Dai balance swap
+    balance = await daiContract.methods.balanceOf(process.env.ACCOUNT).call()
+    balance = web3.utils.fromWei(balance, 'Ether')
+    console.log("Dai Balance:", balance)
 }
 
 let priceMonitor
 let monitoringPrice = false
 
 async function monitorPrice() {
-  if(monitoringPrice) {
-    return
-  }
-
-  console.log("Checking price...")
-  monitoringPrice = true
-
-  try {
-
-    // Check Eth Price
-    const daiAmount = await exchangeContract.methods.getEthToTokenInputPrice(ETH_AMOUNT).call()
-    const price = web3.utils.fromWei(daiAmount.toString(), 'Ether')
-    console.log('Eth Price:', price, ' DAI')
-
-    if(price <= ETH_SELL_PRICE) {
-      console.log('Selling Eth...')
-      // Check balance before sale
-      await checkBalances()
-
-      // Sell Eth
-      await sellEth(ETH_AMOUNT, daiAmount)
-
-      // Check balances after sale
-      await checkBalances()
-
-      // Stop monitoring prices
-      clearInterval(priceMonitor)
+    if(monitoringPrice) {
+      return
     }
 
-  } catch (error) {
-    console.error(error)
-    monitoringPrice = false
-    clearInterval(priceMonitor)
-    return
-  }
+    console.log("Checking price from ChainLink...")
+    monitoringPrice = true
 
-  monitoringPrice = false
+    try {
+
+      // Check Eth Price
+      getEthAggPrice(); // -> this will set the ethPriceFromLinkOracle value
+      let eth2 = ethPriceFromLinkOracle;
+      eth2 = insertDecimal(eth2);
+      console.log('Current ETH price: ', eth2)
+      const daiAmount = await exchangeContract.methods.getEthToTokenInputPrice(ETH_AMOUNT).call()
+      const price = web3.utils.fromWei(daiAmount.toString(), 'Ether')
+      console.log('Eth Price:', eth2, ' DAI')
+
+
+      if(eth2 < ETH_SELL_PRICE) {
+          console.log('Selling Eth...')
+          // Check balance before sale
+          await checkBalances()
+
+          // Sell Eth
+          await sellEth(ETH_AMOUNT, daiAmount)
+
+          // Check balances after sale
+          await checkBalances()
+
+          // Stop monitoring prices
+          clearInterval(priceMonitor)
+      }
+
+    } catch (error) {
+        console.error(error)
+        monitoringPrice = false
+        clearInterval(priceMonitor)
+        return
+    }
+
+    monitoringPrice = false
 }
 
 // Check markets every n seconds
